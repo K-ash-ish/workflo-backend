c0938fd25777ae7426b815259d36f88c9af03dde
K-ash-ish <kashishsondhiya@gmail.com>
2024-07-31 01:58:40 +0530

    db connection ✅

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@
 import dotenv from "dotenv";
 
 import express, { Express } from "express";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-  path: "./.env",
+  path: "./.env"
 });
+
+connectDB();
 
 const app: Express = express();
 const port = process.env.PORT;
