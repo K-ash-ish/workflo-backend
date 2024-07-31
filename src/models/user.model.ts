61d28f08a14294e3c78ab79ed078e623fe096ee0
K-ash-ish <kashishsondhiya@gmail.com>
2024-07-31 12:16:02 +0530

    user modal preHook/ methods

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -13,8 +15,39 @@ const userSchema = new mongoose.Schema(
       required: true,
       lowercase: true,
     },
+    password: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function encryptPassword(next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function checkPassword(
+  password: string
+) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function genAccessToken() {
+  if (!process.env.ACCESS_TOKEN) {
+    throw new Error("Missing env ACCESS TOKEN");
+  }
+  return jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+    },
+    process.env.ACCESS_TOKEN,
+    { expiresIn: "1d" }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
